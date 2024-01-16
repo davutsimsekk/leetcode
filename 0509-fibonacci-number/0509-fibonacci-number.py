b73188a7c33632b5093af891dc7142f3bde5b97d
davutsimsekk <148685165+davutsimsekk@users.noreply.github.com>
2024-01-16 17:39:17 +0300

    Time: 42 ms (52.98%), Space: 17.3 MB (13.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,22 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def solve(n):
+        
+        fiblist=[0,1]
+        
+#         def solve(n):
             
-            if n==1:
-                return 1
-            elif n==0:
-                return 0
+#             if n==1:
+#                 return 1
+#             elif n==0:
+#                 return 0
+#             else:
+                
+#                 return solve(n-1)+solve(n-2)
+        def solve_opt(n):
+            if n<=len(fiblist)-1:
+                return fiblist[n]
             else:
-                return solve(n-1)+solve(n-2)
-        return solve(n)
+                result= solve_opt(n-1)+solve_opt(n-2)
+                fiblist.append(result)
+                return result
+        return solve_opt(n)
