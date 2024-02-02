581dc935b06ce7957b8f3fc138f0335c22d8320e
davutsimsekk <148685165+davutsimsekk@users.noreply.github.com>
2024-02-02 19:58:13 +0000

    Time: 32 ms (78.59%), Space: 16.6 MB (63.61%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,14 +1,18 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
+
         result=[]
-        def solve(number):
-            if str(number)[-1]=="0":
-                return True
+        number=1
+        n=1
+        while number!=9:
+            if n==10:
+                n=1
+                number=int(str(number)[0])+1
+            if str(number)[-1]!=str(n-1):
+                n+=1
             else:
+                number=number*10+n
                 if low<=number<=high:
                     result.append(number)
-                solve(number*10+int(str(number)[-1])+1)
 
-        for i in range(1,10):
-            solve(i)
         return(sorted(result))
