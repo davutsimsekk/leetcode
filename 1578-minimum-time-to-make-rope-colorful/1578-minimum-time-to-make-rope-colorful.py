14a7b750aba0749ebacccf7df675db1a084e8b50
davutsimsekk <148685165+davutsimsekk@users.noreply.github.com>
2024-02-19 20:16:56 +0000

    Time: 849 ms (45.34%), Space: 27.6 MB (87.35%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -6,9 +6,7 @@ class Solution:
         while i!=len(colors):
             if i==len(colors)-1 or colors[i]!=colors[i+1]:
                 stack=neededTime[indextwo+1:i+1]
-                stack.sort()
-                for j in range(len(stack)-1):
-                    res+=stack[j]  
+                res+=sum(stack)-max(stack)  
                 indextwo=i
             i+=1
         return(res)
