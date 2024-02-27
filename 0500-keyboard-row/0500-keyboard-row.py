5b8907d4bec90c4caf5f2352cc5095fb6b651ab9
davutsimsekk <148685165+davutsimsekk@users.noreply.github.com>
2024-02-27 19:40:13 +0000

    Time: 30 ms (86.33%), Space: 16.5 MB (88.16%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,34 +1,11 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        firstrow=['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
-        secondrow=['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
-        thirdrow=['z', 'x', 'c', 'v', 'b', 'n', 'm']
-        result=[]
-        counter=0
+        firstRow = set("qwertyuiopQWERTYUIOP")
+        secondRow = set("asdfghjklASDFGHJKL")
+        thirdRow = set("zxcvbnmZXCVBNM")
+        res = []
         for word in words:
-            addornot=True
-            word=word.lower()
-            ilkharf=word[0]
-            if ilkharf in firstrow:
-                for letter in word[1:]:
-                    if letter not in firstrow:
-                        addornot=False
-                        break
-                if addornot==True:
-                    result.append(words[counter])
-            elif ilkharf in secondrow:
-                for letter in word[1:]:
-                    if letter not in secondrow:
-                        addornot=False
-                        break
-                if addornot==True:
-                    result.append(words[counter])
-            elif ilkharf in thirdrow:
-                for letter in word[1:]:
-                    if letter not in thirdrow:
-                        addornot=False
-                        break
-                if addornot==True:
-                    result.append(words[counter])
-            counter+=1
-        return(result)
+            word_set = set(word)
+            if word_set.issubset(firstRow) or word_set.issubset(secondRow) or word_set.issubset(thirdRow):
+                res.append(word)
+        return res
