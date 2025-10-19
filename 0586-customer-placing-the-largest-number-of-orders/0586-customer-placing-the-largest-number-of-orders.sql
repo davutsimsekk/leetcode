a30a916bec66f576eada05ced249eb06693112cf
Davut Şimşek <148685165+davutsimsekk@users.noreply.github.com>
2025-10-20 00:49:55 +0300

    Time: 439 ms (69.42%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,10 @@
 
-select max(c),customer_number from(
+select customer_number from (
+select customer_number, count(*) from orders
+group by customer_number
+having count(*)=
+(select max(c) from(
     select customer_number,count(*) c from orders
-    group by customer_number) 
+    group by customer_number) as temp )
     
+) as temp2
